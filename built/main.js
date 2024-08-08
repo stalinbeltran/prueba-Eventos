bad47a4d23eb348be79a57b4122310f08b9b52d2
Stalin Beltran <stalin.beltran2006@gmail.com>
2024-08-08 11:01:52 -0500

    Property 'on' does not exist on type 'MyEmitter'

--- built/main.js
+++ built/main.js
@@ -3,11 +3,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const events_1 = __importDefault(require("events"));
 const express_1 = __importDefault(require("express"));
+class MyEmitter extends events_1.default {
+}
 const app = (0, express_1.default)();
 const port = 4000;
 class Demo {
     constructor() {
+        const myEmitter = new MyEmitter();
+        myEmitter.on('event', (a, b) => {
+            console.log(a, b, this);
+            // Prints: a b {}
+        });
+        myEmitter.emit('event', 'a', 'b');
         app.get('/msgrecibido', async (req, res) => {
             console.log('query', req.query);
             res.send(req.query);
--- main.ts
+++ main.ts
@@ -3,11 +3,19 @@ import EventEmitter from "events";
 import express from "express"
 
 
+class MyEmitter extends EventEmitter {}
 const app = express()
 const port = 4000
 
 class Demo{
   constructor(){
+
+    const myEmitter = new MyEmitter();
+    myEmitter.on('event', (a, b) => {
+      console.log(a, b, this);
+      // Prints: a b {}
+    });
+    myEmitter.emit('event', 'a', 'b');
 
     app.get('/msgrecibido', async (req, res) => {
       console.log('query', req.query)
