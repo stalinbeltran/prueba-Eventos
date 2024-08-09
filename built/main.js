77c1165c0a204a7d7b35cbbf0adde01d9e564efa
Stalin Beltran <stalin.beltran2006@gmail.com>
2024-08-09 07:39:44 -0500

    promise return promise (lo q necesito)

--- built/main.js
+++ built/main.js
@@ -53,17 +53,19 @@ class Demo {
     }
     ingresoNombre = async (msg) => {
         new Promise(function (resolve, reject) {
-            let n = 23 * 65;
-            setTimeout(() => resolve(n), 1000); // (*)
+            setTimeout(() => resolve(1), 1000);
         }).then(function (result) {
             console.log(result); // 1
-            return result * 2;
+            return new Promise((resolve, reject) => {
+                setTimeout(() => resolve(result * 2), 1000);
+            });
         }).then(function (result) {
             console.log(result); // 2
-            return result * 2;
+            return new Promise((resolve, reject) => {
+                setTimeout(() => resolve(result * 2), 1000);
+            });
         }).then(function (result) {
             console.log(result); // 4
-            return result * 2;
         });
     };
     mensajeRecibido = (msg) => {
--- main.ts
+++ main.ts
@@ -63,7 +63,31 @@ class Demo{
     }
 
     ingresoNombre = async(msg)=>{
+        new Promise(function(resolve, reject) {
 
+            setTimeout(() => resolve(1), 1000);
+          
+          }).then(function(result:number) {
+          
+            console.log(result); // 1
+          
+            return new Promise((resolve, reject) => { // (*)
+              setTimeout(() => resolve(result * 2), 1000);
+            });
+          
+          }).then(function(result:number) { // (**)
+          
+            console.log(result); // 2
+          
+            return new Promise((resolve, reject) => {
+              setTimeout(() => resolve(result * 2), 1000);
+            });
+          
+          }).then(function(result:number) {
+          
+            console.log(result); // 4
+          
+          });
     }
 
     
